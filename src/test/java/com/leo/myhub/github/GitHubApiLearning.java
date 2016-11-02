3a22a1f1e04696fb26a4730cbea754b00e08ddf5
Leonardo Leite <leonardofl87@gmail.com>
2016-11-02 11:27:47 -0200

    recebendo 401 do github

--- src/test/java/com/leo/myhub/github/GitHubApiLearning.java
+++ src/test/java/com/leo/myhub/github/GitHubApiLearning.java
@@ -1,8 +1,11 @@
 package com.leo.myhub.github;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.json.JSONObject;
 import org.junit.Test;
@@ -16,14 +19,27 @@ public class GitHubApiLearning {
 	@Test
 	public void shouldConsumirApiGithub() {
 		Client client = ClientBuilder.newClient();
-		String returned = client.target("https://api.github.com/")
+		String result = client.target("https://api.github.com/")
 		        .request(MediaType.APPLICATION_JSON)
 		        .get(String.class);
 		client.close();
 		
-		JSONObject json = new JSONObject(returned);
+		JSONObject json = new JSONObject(result);
 		for (String key : json.keySet()) {
 			System.out.println(key + " = " + json.getString(key));
 		}
+	}
+	
+	/**
+	 * É pra acessar esse tipo de informação que precisaremos do OAuth
+	 */
+	@Test
+	public void shouldBeNotAllowed() {
+		Client client = ClientBuilder.newClient();
+		Response response = client.target("https://api.github.com/").path("user").path("keys")
+		        .request(MediaType.APPLICATION_JSON)
+		        .get(Response.class);
+		client.close();
+		assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response.getStatus());
 	}
 }
